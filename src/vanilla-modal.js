9641f6d85e2e29d4c09f3f883ae8ad44a6aef824
Ian Hatton <ian@morsedigital.com>
2016-12-20 11:31:25 +0000

    Initial commit

--- src/vanilla-modal.js
+++ src/vanilla-modal.js
@@ -19,17 +19,26 @@ class ModalClass{
 
   _init(){
     this.open = false;
-    this.overlayClick = this._overlayClick.bind(this);
     this._render();
   }
 
   _render(){
     this._getSource();
     this._getToggle();
+
+    if (this.toggle && this.source){
+      this._createOverlay();
+    }
+  }
+
+  _addCloseClickListener(){
+    this.close.addEventListener('click', this._closeClick.bind(this), false);
   }
 
   _addOverlayClickListener(){
-    this.overlay.addEventListener('click', this.overlayClick, {capture: false, once: true});
+    /* eslint-disable max-len */
+    this.overlay.addEventListener('click', this._overlayClick.bind(this), false);
+    /* eslint-enable */
   }
 
   _addToggleClickListener(){
@@ -40,43 +49,58 @@ class ModalClass{
     document.body.appendChild(this.overlay);
 
     _.delay(()=>{
-      this._toggleModalClass();
-      this._toggleOverlayClass();
+      this._setContentClass();
+      this._setOverlayClass();
     }, 100);
   }
 
-  _overlayClick(e){
+  _closeClick(e){
     e.preventDefault();
 
-    if(e.target === this.overlay){
-      this._toggleModal();
-    }
+    this._setModalVisibility();
   }
 
-  _createModal(){
-    this.modal = document.createElement('div');
+  _createClose(){
+    let span = document.createElement('span');
+    this.close = document.createElement('a');
 
-    this.modal.className += this.config.contentClass;
+    span.innerHTML = 'close';
 
-    this.modal.innerHTML = this.source.innerHTML;
+    this.close.className = this.config.closeClass;
+    this.close.setAttribute('href', '#');
+    this.close.appendChild(span);
 
-    this.overlay.appendChild(this.modal);
+    this._addCloseClickListener();
+
+    this.content.appendChild(this.close);
+  }
+
+  _createContent(){
+    this.content = document.createElement('div');
+
+    this.content.className = this.config.contentClass;
+
+    this.content.innerHTML = this.source.innerHTML;
+
+    this._createClose();
+
+    this.overlay.appendChild(this.content);
   }
 
   _createOverlay(){
     this.overlay = document.createElement('div');
 
-    this.overlay.className += this.config.overlayClass;
+    this.overlay.className = this.config.overlayClass;
 
-    this._createModal();
+    this._addOverlayClickListener();
+
+    this._createContent();
   }
 
   _getSource(){
     /* eslint-disable max-len */
     this.source = this.config.element.querySelector('.' + this.config.sourceClass);
     /* eslint-enable */
-
-    this._createOverlay();
   }
 
   _getToggle(){
@@ -87,72 +111,87 @@ class ModalClass{
     this._addToggleClickListener();
   }
 
+  _lazyLoadImages(){
+    let images = this.content.querySelectorAll('img');
+
+    _.forEach(images, (image)=>{
+      let dataSrc = image.getAttribute('data-src');
+      let src = image.getAttribute('src');
+
+      if (dataSrc && _.isEmpty(src)){
+        image.src = dataSrc;
+      }
+    });
+  }
+
+  _overlayClick(e){
+    if (e.target === this.overlay){
+      e.preventDefault();
+
+      this._setModalVisibility();
+    }
+  }
+
   _removeOverlay(){
-    this._toggleOverlayClass();
-    this._toggleModalClass();
+    this._setOverlayClass();
+    this._setContentClass();
 
     _.delay(()=>{
       document.body.removeChild(this.overlay);
     }, 500);
   }
 
-  _setModalPosition(){
-    let position = 40;
+  _setContentClass(){
+    let className = this.content.className;
 
-    this.modal.style.top = position + 'px';
-  }
-
-  _toggleBodyClass(){
-    let className = document.body.className;
-
-    if(_.includes(className, 'modal-open')){
-      document.body.className = className.replace(/(?:^|\s)modal-open(?!\S)/g, '');
+    if (_.includes(className, 'active')){
+      this.content.className = className.replace(/(?:^|\s)active(?!\S)/g, '');
     } else {
-      document.body.className += ' modal-open';
+      this.content.className += ' active';
     }
   }
 
-  _toggleClick(e){
-    e.preventDefault();
+  _setHTMLClass(){
+    let html = document.getElementsByTagName('html')[0];
+    let className = html.className;
 
-    this._toggleModal();
+    if (_.includes(className, 'modal-open')){
+      html.className = className.replace(/(?:^|\s)modal-open(?!\S)/g, '');
+    } else {
+      html.className += ' modal-open';
+    }
   }
 
-  _toggleOverlayClass(){
+  _setModalVisibility(){
+    if (this.open){
+      this._removeOverlay();
+    } else {
+      this._appendOverlay();
+    }
+
+    this._setHTMLClass();
+    this._setOpenState();
+  }
+
+  _setOpenState(){
+    this.open = !this.open;
+  }
+
+  _setOverlayClass(){
     let className = this.overlay.className;
 
-    if(_.includes(className, 'active')){
+    if (_.includes(className, 'active')){
       this.overlay.className = className.replace(/(?:^|\s)active(?!\S)/g, '');
     } else {
       this.overlay.className += ' active';
     }
   }
 
-  _toggleModalClass(){
-    let className = this.modal.className;
+  _toggleClick(e){
+    e.preventDefault();
 
-    if(_.includes(className, 'active')){
-      this.modal.className = className.replace(/(?:^|\s)active(?!\S)/g, '');
-    } else {
-      this.modal.className += ' active';
-    }
-  }
-
-  _toggleModal(){
-    if(this.open){
-      this._removeOverlay();
-    } else {
-      this._setModalPosition();
-      this._appendOverlay();
-      this._addOverlayClickListener();
-    }
-
-    this._toggleBodyClass();
-    this._toggleOpenState();
-  }
-
-  _toggleOpenState(){
-    this.open = !this.open;
+    this._lazyLoadImages();
+    this._setModalVisibility();
   }
 }
 
